2c43f7c43fd9b6f4c6705ddfa915d14a2cba544c
roland <roland25@gmail.com>
2017-04-04 22:05:14 +0200

    init

---
+++ ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 2017-03-21 19:47:38
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
